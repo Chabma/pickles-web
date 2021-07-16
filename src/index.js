5f3b3c1e51f85b8067bd664ae20403dcc7a02993
Chabma <Chabma@gmail.com>
2021-07-16 17:23:11 -0400

    fix hash route pt 5

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
