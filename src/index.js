d83d640fc6e8a6427aee071d185be64ca7adaf9a
Chabma <Chabma@gmail.com>
2021-07-16 15:47:24 -0400

    update github pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
